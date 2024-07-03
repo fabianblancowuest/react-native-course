91af128c20560f89a522187a3853c5e5bf5578b8
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-02 21:50:05 -0300

    Paginación de resultados

--- 01-react-improvement/src/components/UsersPage.tsx
+++ 01-react-improvement/src/components/UsersPage.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { ReqResUserListResponse, User } from "../interfaces";
 
-const loadUsers = async (): Promise<User[]> => {
+const loadUsers = async (page: number = 1): Promise<User[]> => {
 	try {
 		const { data } = await axios.get<ReqResUserListResponse>(
 			"https://reqres.in/api/users",
+			{
+				params: {
+					page: page,
+				},
+			},
 		);
 
 		return data.data;
@@ -17,16 +22,41 @@ const loadUsers = async (): Promise<User[]> => {
 
 export const UsersPage = () => {
 	const [users, setUsers] = useState<User[]>([]);
+	const currentPageRef = useRef(1);
 
 	useEffect(() => {
-		loadUsers().then(setUsers);
+		loadUsers(currentPageRef.current).then(setUsers);
 	}, []);
 
+	const nextPage = async () => {
+		currentPageRef.current++;
+
+		const users = await loadUsers(currentPageRef.current);
+
+		if (users.length > 0) {
+			setUsers(users);
+		} else {
+			currentPageRef.current--;
+		}
+		loadUsers(currentPageRef.current);
+		console.log(currentPageRef.current);
+	};
+
+	const prevPage = async () => {
+		if (currentPageRef.current <= 1) {
+			return;
+		}
+		currentPageRef.current--;
+
+		const users = await loadUsers(currentPageRef.current);
+
+		setUsers(users);
+	};
 	return (
 		<>
 			<h3>Usuarios:</h3>
 			<table>
-				<thead>
+				<thead className="userColumnTitle">
 					<tr>
 						<th>Avatar</th>
 						<th>Nombre</th>
@@ -34,12 +64,25 @@ export const UsersPage = () => {
 					</tr>
 				</thead>
 
-				<tbody>
+				<tbody className="usersInfo">
 					{users.map((user, index) => {
 						return <UsersRow key={index} user={user}></UsersRow>;
 					})}
 				</tbody>
 			</table>
+
+			<div className="buttons">
+				<button
+					className="btn-1"
+					onClick={prevPage}
+					disabled={currentPageRef.current <= 1}
+				>
+					Prev
+				</button>
+				<button className="btn-2" onClick={nextPage}>
+					Next
+				</button>
+			</div>
 		</>
 	);
 };
@@ -52,14 +95,14 @@ export const UsersRow = ({ user }: Props) => {
 	const { avatar, first_name, last_name, email } = user;
 
 	return (
-		<tr>
-			<td>
+		<tr className="userInfo">
+			<td className="userColumn">
 				<img src={avatar} alt="User Avatar" />
 			</td>
-			<td>
+			<td className="userColumn">
 				{first_name} {last_name}
 			</td>
-			<td>{email}</td>
+			<td className="userColumn">{email}</td>
 		</tr>
 	);
 };
