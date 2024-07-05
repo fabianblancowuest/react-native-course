c39273cd252824415fdd61e19d80ce65af544f28
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-04 23:45:20 -0300

    Crear un contador

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
+import {CounterScreen} from './src/presentation/screens/CounterScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <HelloWorldScreen name="Fabián Blanco Wuest" />
+      {/* <HelloWorldScreen name="Fabián Blanco Wuest" /> */}
+      <CounterScreen />
     </SafeAreaView>
   );
 };
