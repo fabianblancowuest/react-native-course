3d6ad6b59298ebca2f02cfe51a5b09c5e9ad1dbf
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-08 19:22:28 -0300

    Posición absoluta

--- 02-MyFirstApp/src/presentation/screens/PositionScreen.tsx
+++ 02-MyFirstApp/src/presentation/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ export const PositionScreen = () => {
   return (
     <View style={styles.container}>
       {/* <Text style={styles.text}>Position Screen</Text> */}
+      <View style={styles.greenBox}></View>
       <View style={styles.purpleBox}></View>
       <View style={styles.orangeBox}></View>
     </View>
@@ -16,8 +17,9 @@ const styles = StyleSheet.create({
     // height: 150,
     // width: 300,
     backgroundColor: '#28c4d9',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    position: 'relative',
   },
 
   text: {
@@ -33,7 +35,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856d6',
     borderWidth: 10,
     borderColor: 'white',
-    top: -0,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
   },
   orangeBox: {
     width: 100,
@@ -41,7 +45,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0a23b',
     borderWidth: 10,
     borderColor: 'white',
+    position: 'absolute',
     top: 0,
-    left: -0,
+    right: 0,
+  },
+
+  greenBox: {
+    flex: 1,
+    // width: '100%',
+    // height: 100,
+    backgroundColor: '#3bf07a',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // top: 0,
+    // right: 0,
+    // bottom: 0,
+    // left: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
