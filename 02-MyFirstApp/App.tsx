2a9cd7982fc6b135c284cce1f815d6a55ea5c7c3
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-16 15:17:21 -0300

    Preparación de la tarea

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -10,6 +10,7 @@ import {DimensionScreen} from './src/presentation/screens/DimensionScreen';
 import {PositionScreen} from './src/presentation/screens/PositionScreen';
 import {FlexScreen} from './src/presentation/screens/FlexScreen';
 import {FlexDirectionScreen} from './src/presentation/screens/FlexDirectionScreen';
+import {HomeworkScreen} from './src/presentation/screens/HomeworkScreen';
 
 export const App = () => {
   return (
@@ -25,7 +26,8 @@ export const App = () => {
         {/* <DimensionScreen /> */}
         {/* <PositionScreen /> */}
         {/* <FlexScreen /> */}
-        <FlexDirectionScreen />
+        {/* <FlexDirectionScreen /> */}
+        <HomeworkScreen />
       </SafeAreaView>
     </PaperProvider>
   );
