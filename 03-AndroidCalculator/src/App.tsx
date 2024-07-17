941ec4c333ac8c150fca376514828e383691447f
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-17 17:09:21 -0300

    Diseño de pantalla y estilos globales

--- 03-AndroidCalculator/src/App.tsx
+++ 03-AndroidCalculator/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {StatusBar, View} from 'react-native';
 import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+import {globalStyles} from './config/theme/app-theme';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -9,7 +10,7 @@ type SectionProps = PropsWithChildren<{
 
 function App() {
   return (
-    <View>
+    <View style={globalStyles.background}>
       <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
       <CalculatorScreen />
     </View>
--- 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {globalStyles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text>CalculatorScreen</Text>
+    <View style={globalStyles.calculatorContainer}>
+      <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
+        <Text style={globalStyles.mainResult}>1500</Text>
+        <Text style={globalStyles.subResult}>15</Text>
+      </View>
     </View>
   );
 };
