a7f19eeb33f2fef0425539cd308839c35f6a26a0
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-17 18:02:28 -0300

    Botones y estilos

--- 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import {globalStyles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -8,6 +8,12 @@ export const CalculatorScreen = () => {
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
         <Text style={globalStyles.mainResult}>1500</Text>
         <Text style={globalStyles.subResult}>15</Text>
+      </View>
+
+      <View style={globalStyles.row}>
+        <Pressable style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>1</Text>
+        </Pressable>
       </View>
     </View>
   );
