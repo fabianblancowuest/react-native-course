27ef7cecdc854dd1244c3eb4f920b6924fa5e24c
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-18 20:58:52 -0300

    Botón de calculadora -tarea-

--- 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ 03-AndroidCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -17,6 +17,30 @@ export const CalculatorScreen = () => {
         <CalculatorButton label="del" color={colors.darkGray} />
         <CalculatorButton label="/" color={colors.orange} />
       </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="7" color={colors.darkGray} />
+        <CalculatorButton label="8" color={colors.darkGray} />
+        <CalculatorButton label="9" color={colors.darkGray} />
+        <CalculatorButton label="X" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="4" color={colors.darkGray} />
+        <CalculatorButton label="5" color={colors.darkGray} />
+        <CalculatorButton label="6" color={colors.darkGray} />
+        <CalculatorButton label="-" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="1" color={colors.darkGray} />
+        <CalculatorButton label="2" color={colors.darkGray} />
+        <CalculatorButton label="3" color={colors.darkGray} />
+        <CalculatorButton label="+" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="0" color={colors.darkGray} />
+        <CalculatorButton label="." color={colors.darkGray} />
+        {/* <CalculatorButton label="" color={colors.darkGray} /> */}
+        <CalculatorButton label="=" color={colors.orange} />
+      </View>
     </View>
   );
 };
