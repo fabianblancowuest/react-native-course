a0b95291f42cfdcdfe0d67a920b5dcf736c5ab87
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-23 20:24:25 -0300

    Archivos y directorios del proyecto

---
+++ 04 - NavigationApp/src/presentation/screens/settings/SettingScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const SettingScreen = () => {
+  return (
+    <Text>
+      <View>SettingScreen</View>
+    </Text>
+  );
+};
