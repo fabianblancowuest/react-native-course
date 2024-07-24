f3ea28e896e111c7072a66fd26561092d480c636
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-23 21:06:31 -0300

    React Navigation - Pre-requisitos

--- 04 - NavigationApp/src/App.tsx
+++ 04 - NavigationApp/src/App.tsx
@@ -1,10 +1,11 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {Text, View} from 'react-native';
 
 export const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>App.tsx</Text>
-    </View>
+    </NavigationContainer>
   );
 };
